bce79c26514cdc94ebfe688a4b4faac4d2babcc8
AntonGusev76 <antongus76@gmail.com>
2025-05-09 20:40:45 +0400

    first commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import telebot
+import random
+import string
+
+TOKEN = '7243602173:AAHzqBdzqjxCKfxvhz-IxFmJlzQTuY7UzOA'
+bot = telebot.TeleBot(TOKEN)
+
+def generate_password(length=12):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(random.choice(chars) for _ in range(length))
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+    for length in [8, 12, 16]:
+        markup.add(telebot.types.KeyboardButton(str(length)))
+    bot.send_message(message.chat.id, "Выбери длину пароля:", reply_markup=markup)
+
+@bot.message_handler(func=lambda message: message.text.isdigit())
+def send_password(message):
+    length = int(message.text)
+    if length < 4:
+        bot.reply_to(message, "Минимум 4 символа.")
+    else:
+        password = generate_password(length)
+        bot.reply_to(message, f"Вот твой пароль:\n`{password}`", parse_mode='Markdown')
+
+bot.polling()
